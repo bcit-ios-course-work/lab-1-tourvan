9637f0d9c09f28b1f79b68b4a5dc99cb9c2b0430
Aiste Ulozaite <aiste.ulozaite@gmail.com>
2018-09-30 12:04:15 -0700

    Experimenting with creating and saving plist - unsuccessful

--- tourvan/DetailViewController.swift
+++ tourvan/DetailViewController.swift
@@ -51,10 +51,47 @@ class DetailViewController: UIViewController {
         "description":"Science World at Telus World of Science, Vancouver is a science centre run by a not-for-profit organization in Vancouver, British Columbia, Canada. It is located at the end of False Creek, and features many permanent interactive exhibits and displays, as well as areas with varying topics throughout the years. "
     ]
     
+    let dataPlistUrl = Bundle.main.url(forResource: "Data", withExtension: "plist")
+    let fileManager = FileManager.default
+    let dataArray = NSArray()
+    
+//    let dataArray =
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         var attractions:[[String: String]] = [grouse, museum, whistler, lookout, gastown, scienceworld];
+        
+        // 1. get access to the file
+        // 2. create object
+        // 3. write to the file
+        
+        // Remove after first run
+        print("Starting to generate pList")
+        for i in (0...attractions.count-1) {
+            
+            let attractionImage = UIImage(named: attractions[i]["image"]!)
+            let attractionData = UIImage.pngData(attractionImage!)
+            let attractionName = attractions[i]["name"]!
+            let attractionDescription = attractions[i]["description"]!
+            
+            let object = ["name" : attractionName, "description" : attractionDescription, "image" :  attractionData ] as [String : Any]
+            print("Adding object to array")
+            dataArray.adding(object)
+        }
+        
+        print("done adding to dataArray \(dataArray)")
+
+        let serializedData = try! PropertyListSerialization.data(fromPropertyList: dataArray, format: PropertyListSerialization.PropertyListFormat.xml, options: 0)
+        print("serializedData")
+        let document = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first
+        print("document")
+
+        let logFileURL = document?.appendingPathComponent("attractionData.plist")
+        print("logFileURL")
+
+       try! serializedData.write(to: logFileURL!, options: [])
+        print("write")
         let attraction = UserDefaults.standard.object(forKey: "location")
 
         for i in (0...attractions.count-1) {
